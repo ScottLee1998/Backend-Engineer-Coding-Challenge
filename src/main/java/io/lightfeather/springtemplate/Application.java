68c1b1a161b57ef3ed8222e939158884c10d3a6d
Scott-Lee-Regional <scott@finalityllc.com>
2024-12-16 20:24:05 -0500

    Modified get endpoint to use configurable value

--- src/main/java/io/lightfeather/springtemplate/Application.java
+++ src/main/java/io/lightfeather/springtemplate/Application.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,16 +28,19 @@ import io.lightfeather.springtemplate.model.Supervisor;
 @RestController
 public class Application {
 
+  @Value("${managers.source.endpoint}")
+  private String source;
+
   @RequestMapping("/")
   public String home() {
     return "Hello World";
   }
 
   @GetMapping("/api/supervisors")
-  public List<String> getMethodName() throws URISyntaxException, MalformedURLException, IOException {
+  public List<String> getMethodName() throws URISyntaxException, IOException {
 
     // make get request
-    URI uri = new URI("https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers");
+    URI uri = new URI(source);
     HttpURLConnection con = (HttpURLConnection) uri.toURL().openConnection();
     con.setRequestMethod("GET");
     InputStream inputStream = con.getInputStream();
