6b9397bce072240b50ccca858c61b4b9dc9deae9
Scott-Lee-Regional <scott@finalityllc.com>
2024-12-16 20:59:49 -0500

    Added post endpoint

--- src/main/java/io/lightfeather/springtemplate/Application.java
+++ src/main/java/io/lightfeather/springtemplate/Application.java
@@ -14,8 +14,12 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -67,7 +71,34 @@ public class Application {
       output.add(supervisor.toString()); 
     }
     return output;
-  } 
+  }
+
+  @PostMapping("/api/submit")
+  public ResponseEntity<String> createUser(@RequestParam(required = true) String firstName, @RequestParam(required = true) String lastName, @RequestParam(required = false) String email, @RequestParam(required = false) String phoneNumber, @RequestParam(required = true) String supervisor) {
+    StringBuilder stringBuilder = new StringBuilder();
+
+    // return error status code if required parameters are empty
+    if(firstName.isBlank() || lastName.isBlank() || supervisor.isBlank()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing required data");
+    }
+    // write params to console if present
+    else {
+      stringBuilder.append("First Name: " + firstName);
+      stringBuilder.append(", Last Name: " + lastName);
+      stringBuilder.append(", Supervisor: " + supervisor);
+      if(email != null && !email.isBlank()) {
+        stringBuilder.append(", Email: " + email);
+      }
+      if(phoneNumber != null && !phoneNumber.isBlank()) {
+        stringBuilder.append(", Phone Number: " + phoneNumber);
+      }
+      System.console().printf(stringBuilder.toString() + "\n");
+
+      // return success status code
+      return ResponseEntity.status(HttpStatus.OK).body(stringBuilder.toString());
+    }
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
